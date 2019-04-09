5fe07f82ccba7fbd3494ea0b9adf110bd6ff4541
sagarkharbe <sagarkharabe97@gmail.com>
2019-04-09 17:25:08 +0530

    registeration test complete through postman

---
+++ models/Users.js
@@ -0,0 +1,24 @@
+const userSchema = new require("mongoose").Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = require("mongoose").model("Users", userSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,32 @@
 const router = require("express").Router();
+const brcypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const { User } = require("../../models/index");
+router.post("/register", (req, res) => {
+  const { email, password, name } = req.body;
+  if (!email || !password || !name) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(401).json({ msg: "Email already registered" });
+    }
+    const newUser = new User({
+      name,
+      email,
+      password,
+      avatar
+    });
+    brcypt.genSalt(10, (err, salt) => {
+      brcypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then(user => {
+          res.json(user);
+        });
+      });
+    });
+  });
+});
 
-router.get("/", (req, res) => res.send("api/users"));
 module.exports = router;
