a7e75235e6f49d6268b03aa37c7f58ad4365c499
sagarkharbe <sagarkharabe97@gmail.com>
2019-04-22 16:01:16 +0530

    added routes and register and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Login";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
+        <Router>
+          <Navbar />
+          <Route path="/" exact component={Landing} />
+          <div className="container">
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />
+          </div>
+          <Footer />
+        </Router>
       </div>
     );
   }
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Register extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    );
+  }
+}
+
+export default Register;
