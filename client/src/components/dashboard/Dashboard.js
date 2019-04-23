0aa1622f065fe2893418c92c096e8a7e130ba41a
sagarkharbe <sagarkharabe97@gmail.com>
2019-04-23 17:49:08 +0530

    added a spinner while the profile is loading

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,17 +2,55 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profileAction";
+import Spinner from "../common/Spinner";
+import { Link } from "react-router-dom";
 class Dashboard extends Component {
-  static propTypes = {};
+  static propTypes = {
+    profile: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
+    getCurrentProfile: PropTypes.func.isRequired
+  };
   componentDidMount() {
     this.props.getCurrentProfile();
   }
   render() {
-    return <div />;
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+    let dashboardContent;
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />;
+    } else if (Object.keys(profile).length > 0) {
+      dashboardContent = <h4>Display Profile</h4>;
+    } else {
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">Welcome {user.name}</p>
+          <p>You have not yet setup a profile , please add some info</p>
+          <Link to="/create-profile" className="btn btn-lg btn-info">
+            Create Profile
+          </Link>
+        </div>
+      );
+    }
+    return (
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
+const mapStateToProps = ({ auth, profile }) => {
+  return { profile, auth };
+};
 
 export default connect(
-  null,
+  mapStateToProps,
   { getCurrentProfile }
 )(Dashboard);
