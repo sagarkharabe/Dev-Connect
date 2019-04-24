099948b1cec08e1dd0271a60ff1abd597857ce55
sagarkharbe <sagarkharabe97@gmail.com>
2019-04-24 17:33:37 +0530

    added react moment

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
-
+import moment from "react-moment";
 class Experience extends Component {
   static propTypes = {};
   render() {
