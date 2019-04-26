c984f18ab34ef6670a2750c8178e893a370538ed
sagarkharbe <sagarkharabe97@gmail.com>
2019-04-26 14:33:34 +0530

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const chalk = require("chalk");
 const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const passport = require("passport");
@@ -19,6 +20,13 @@ mongoose
 app.use(passport.initialize());
 
 app.use("/api", require("./routes"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5001;
 
 app.listen(port, () =>
